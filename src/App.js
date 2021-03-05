3eebed962960c711e6528b93a391e90a92e53348
Mateusz <matt.borowczyk@gmail.com>
2021-03-05 21:34:54 +0100

    added modules to App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import Shop from "./Shop";
-import Cart from "./Cart";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Item from './components/Item';
+import Cart from './components/Cart';
 import "./styles/app.css";
 
 const App = () => {
+  const [cartItems, setCartItems] = useState([])
+
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">ORIOLE</Link>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/shop">Shop</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className='separator'></div>
+      <Navbar number={cartItems.length}/>
+     <main className="App">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/shop" component={Shop} />
           <Route exact path="/cart" component={Cart} />
         </Switch>
-      </div>
+      </main>
     </Router>
   );
 };
