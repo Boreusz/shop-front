d5910261705bd2d9d96d512159e3d6ac87906d3d
Mateusz <matt.borowczyk@gmail.com>
2021-03-07 17:17:36 +0100

    data for items

--- src/data/items.js
+++ src/data/items.js
@@ -1,5 +1,37 @@
-export default [
-    {
+/* eslint-disable import/no-anonymous-default-export */
+import image1  from '../images/001.jpg'
+import image2  from '../images/002.jpg'
+import image3  from '../images/003.jpg'
+import image4  from '../images/004.jpg'
 
-    }
+
+export default [
+  {
+    id: "001",
+    name: "Nerve Plant",
+    category: "small",
+    image: image1,
+    price: '23.99$'
+  },
+  {
+    id: "002",
+    name: "Desert Rose",
+    category: "small",
+    image: image2,
+    price: '29.99$'
+  },
+  {
+    id: "003",
+    name: "Corkscrew Albuca",
+    category: "small",
+    image: image3,
+    price: '39.99$'
+  },
+  {
+    id: "004",
+    name: "Variegated Monstera Deliciosa",
+    category: "medium",
+    image: image4,
+    price: '239.99$'
+  },
 ];
