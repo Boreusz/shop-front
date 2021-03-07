355029f3af46eaa1de49e6ac8ad5ab75e407abb9
Mateusz <matt.borowczyk@gmail.com>
2021-03-07 17:16:50 +0100

    state addition to prind selected items and props to cart shop and item page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Shop from './components/Shop';
-import Item from './components/Item';
-import Cart from './components/Cart';
+import React, { useEffect, useState} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+import Item from "./components/Item";
 import "./styles/app.css";
 
 const App = () => {
-  const [cartItems, setCartItems] = useState([])
-
+  const [cartItems, setCartItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState('000')
+  const selectItem = (id) =>{
+    setSelectedItem(id)
+  }
   return (
     <Router>
-      <Navbar number={cartItems.length}/>
-     <main className="App">
+      <div >
+      <Navbar number={cartItems.length} />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/shop" component={Shop} />
-          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/shop" render={() => <Shop setSelectedItem={selectItem} />} />
+          <Route path="/cart" render={(props) => <Cart {...props} />} />
+          <Route path="/shop/:id" render={() => <Item item={selectedItem} />} />
         </Switch>
-      </main>
+      </div>
     </Router>
   );
 };
