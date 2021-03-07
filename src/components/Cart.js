3857530e010f9f094b74e5cb94d1e0765c66663a
Mateusz <matt.borowczyk@gmail.com>
2021-03-07 19:09:11 +0100

    cleaning

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "../styles/cart.css"
 
 const Cart = () => {
   return (
-    <div>
-      <h1>Hello from Cart</h1>
+    <div className="cart_container">
+      <div className="cart_wrapper">
+        <h1>Coming Soon</h1>
+      </div>
     </div>
   );
 };
