25161fa4cbcdfbe4b5556e17c887161f2b5a56b8
Mateusz <matt.borowczyk@gmail.com>
2021-03-09 20:03:43 +0100

    filter logic, visual changes to background

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import data from "../data/items";
 import "../styles/shop.css";
 
 const Shop = (props) => {
+  const [dataToPrint, setData] = useState(data);
+  const filterData = (category) => {
+    if (category === "all") {
+      setData(data);
+    } else {
+      let newData = data.filter(item => item.category === category)
+      setData(newData)
+    }
+  };
   useEffect(() => {
     document.title = "Oriole - Shop";
   });
@@ -12,40 +21,56 @@ const Shop = (props) => {
       <div className="shop_wrapper">
         <aside className="shop_side">
           <h2>Categories</h2>
-          <ul className='side_list'>
-            <li className='side_list-element'>
-              <Link to='/shop'>All</Link>
+          <ul className="side_list">
+            <li className="side_list-element">
+              <button className="navi-bttn" type="button" onClick={() => filterData("all")}>
+                All
+              </button>
             </li>
-             <li className='side_list-element'>
-              <Link to='/shop'>Small</Link>
+            <li className="side_list-element">
+              <button className="navi-bttn" type="button" onClick={() => filterData("small")}>
+                Small
+              </button>
             </li>
-            <li className='side_list-element'>
-              <Link to='/shop'>Medium</Link>
+            <li className="side_list-element">
+              <button className="navi-bttn" type="button" onClick={() => filterData("medium")}>
+                Medium
+              </button>
             </li>
-            <li className='side_list-element'>
-              <Link to='/shop'>Big</Link>
+            <li className="side_list-element">
+              <button className="navi-bttn" type="button" onClick={() => filterData("big")}>
+                Big
+              </button>
             </li>
           </ul>
         </aside>
         <main className="shop_main">
-          <ul className='items_list'>
-            {data.map((item) => {
-              return (
-                <li key={item.id} className='items_list-element' onClick={() => props.setSelectedItem(item)}>
-                  <Link to={`/shop/${item.id}`} className='element-link'>
-                    <div>
-                      <img
-                        className="element-image"
-                        src={item.image}
-                        alt={item.name}
-                      />
-                      <div className="element-name">{item.name}</div>
-                      <div className="element-price">{item.price}</div>
-                    </div>
-                  </Link>
-                </li>
-              );
-            })}
+          <ul className="items_list">
+            {dataToPrint.length ? (
+              dataToPrint.map((item) => {
+                return (
+                  <li
+                    key={item.id}
+                    className="items_list-element"
+                    onClick={() => props.setSelectedItem(item)}
+                  >
+                    <Link to={`/shop/${item.id}`} className="element-link">
+                      <div>
+                        <img
+                          className="element-image"
+                          src={item.image}
+                          alt={item.name}
+                        />
+                        <div className="element-name">{item.name}</div>
+                        <div className="element-price">{item.price}</div>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })
+            ) : (
+              <div className="empty">There is no Plants in this category. <br/> Check again in a few days or contact us! </div>
+            )}
           </ul>
         </main>
       </div>
