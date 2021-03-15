8c0d1e78cc400db809b233ff4afc7aa0c1bb9af7
Mateusz <matt.borowczyk@gmail.com>
2021-03-15 19:37:21 +0100

    formating/queries for responsiveness

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,15 +5,15 @@ import "../styles/shop.css";
 
 const Shop = (props) => {
   const [dataToPrint, setData] = useState(data);
-  const [currentFilter, setCurrentFilter] = useState('All')
+  const [currentFilter, setCurrentFilter] = useState("All");
   const filterData = (category) => {
     if (category === "all") {
       setData(data);
     } else {
-      let newData = data.filter(item => item.category === category)
-      setData(newData)
+      let newData = data.filter((item) => item.category === category);
+      setData(newData);
     }
-    setCurrentFilter(category[0].toUpperCase() + category.slice(1))
+    setCurrentFilter(category[0].toUpperCase() + category.slice(1));
   };
   useEffect(() => {
     document.title = "Oriole - Shop";
@@ -22,25 +22,43 @@ const Shop = (props) => {
     <div className="shop_container">
       <div className="shop_wrapper">
         <aside className="shop_side">
-          <h2 className="shop_side-title">Products/<span>{currentFilter}</span></h2>
+          <h2 className="shop_side-title">
+            Products/<span>{currentFilter}</span>
+          </h2>
           <ul className="side_list">
             <li className="side_list-element">
-              <button className="navi-bttn" type="button" onClick={() => filterData("all")}>
+              <button
+                className="navi-bttn"
+                type="button"
+                onClick={() => filterData("all")}
+              >
                 All
               </button>
             </li>
             <li className="side_list-element">
-              <button className="navi-bttn" type="button" onClick={() => filterData("small")}>
+              <button
+                className="navi-bttn"
+                type="button"
+                onClick={() => filterData("small")}
+              >
                 Small
               </button>
             </li>
             <li className="side_list-element">
-              <button className="navi-bttn" type="button" onClick={() => filterData("medium")}>
+              <button
+                className="navi-bttn"
+                type="button"
+                onClick={() => filterData("medium")}
+              >
                 Medium
               </button>
             </li>
             <li className="side_list-element">
-              <button className="navi-bttn" type="button" onClick={() => filterData("big")}>
+              <button
+                className="navi-bttn"
+                type="button"
+                onClick={() => filterData("big")}
+              >
                 Big
               </button>
             </li>
@@ -71,7 +89,10 @@ const Shop = (props) => {
                 );
               })
             ) : (
-              <div className="empty">There is no Plants in this category. <br/> Check again in a few days!</div>
+              <div className="empty">
+                There is no Plants in this category. <br /> Check again in a few
+                days!
+              </div>
             )}
           </ul>
         </main>
